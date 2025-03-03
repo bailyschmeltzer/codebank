4a8acc59708ce9945c446756a019a23d06c227ff
bailyschmeltzer <cards45and12@gmail.com>
2025-03-03 10:16:16 -0500

    Update BaseConfig.bat
    Removed username information

--- BaseConfig.bat
+++ BaseConfig.bat
@@ -3,7 +3,7 @@ net user Administrator active:no
 WMIC USERACCOUNT WHERE Name='Administrator' SET PasswordExpires=FALSE
 net user USERNAME PASSWORD /add
 WMIC USERACCOUNT WHERE Name='USERNAME' SET PasswordExpires=FALSE
-net localgroup administrators MITLocal /add
+net localgroup administrators USERNAME /add
 reg add "HKLM\SYSTEM\CurrentControlSet\Control\Terminal Server" /v fDenyTSConnections /T REG_DWORD /d 0 /f
 reg add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System /v EnableLUA /t REG_DWORD /d 0 /f 
 reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Terminal Server\WinStations\RDP-TCP" /v UserAuthentication /t REG_DWORD /d 0 /f
