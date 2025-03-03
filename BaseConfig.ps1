1e9844e8a5cac84f9026fb3a68e634b74ec79b20
bailyschmeltzer <cards45and12@gmail.com>
2025-03-03 15:03:09 -0500

    Update BaseConfig.ps1
    Corrected misspelling

--- BaseConfig.ps1
+++ BaseConfig.ps1
@@ -19,7 +19,7 @@ powercfg /change standby-timeout-ac 0
 powercfg /change standby-timeout-dc 0
 powercfg /change monitor-timeout-ac 0
 powercfg /change monitor-timeout-dc 0
-powercfg /hange hibernate-timeout-ac 0
+powercfg /change hibernate-timeout-ac 0
 powercfg /change hibernate-timeout-dc 0
 
 
