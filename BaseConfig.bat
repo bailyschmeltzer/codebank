3916ac0461362c3ec9e1ce031e4470e7782b007f
bailyschmeltzer <cards45and12@gmail.com>
2025-03-04 09:07:22 -0500

    Update BaseConfig.bat
    Began moving installers to ps1 file

--- BaseConfig.bat
+++ BaseConfig.bat
@@ -10,29 +10,9 @@ reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Terminal Server\Win
 netsh AdvFirewall set allprofiles state off
 net localgroup Administrators "Domain Users" /add
 "c:\BaseConfig\365install.exe" /configure "c:\BaseConfig\365config.xml"
-"c:\BaseConfig\chromeinstall.msi" /quiet /passive
-"c:\BaseConfig\dellcommandinstall.exe"
+
 "c:\BaseConfig\adobeinstall.exe"
 dism /online /Import-DefaultAppAssociations:"c:\BaseConfig\AppAssociations.xml"
-
-
-# Download the Teams bootstrapper if not already present 
-$downloadUrl = "https://download.microsoft.com/download/7/0/5/70585c-c22b-4bcb-b69d-14e2d4c038a2/Teams_windows_x64.exe" 
-$localPath = "C:\Temp\TeamsBootstrapper.exe" 
-
-# Checks if .exe already exists in file path, downloads if not
-if(!(Test-Path $localPath)) {
-    Invoke-WebRequest -Uri $downloadUrl -OutFile $localPath -UseBasicParsing
-}
-
-
-# Remove existing copies of Teams
-teamsbootstrapper -x
-teamsbootstrapper -u 
-
-# Install Teams using the bootstrapper 
-Start-Process -FilePath $localPath -ArgumentList "-p" -Wait
-
 
 
 powershell.exe set-executionpolicy remotesigned
