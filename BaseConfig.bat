94db38b339192976e48e0462b61a05af25b95c5f
bailyschmeltzer <cards45and12@gmail.com>
2025-03-04 09:09:38 -0500

    Update BaseConfig.bat
    Further conversion to ps1

--- BaseConfig.bat
+++ BaseConfig.bat
@@ -1,14 +1,3 @@
-net user Administrator PASSWORD
-net user Administrator active:no
-WMIC USERACCOUNT WHERE Name='Administrator' SET PasswordExpires=FALSE
-net user USERNAME PASSWORD /add
-WMIC USERACCOUNT WHERE Name='USERNAME' SET PasswordExpires=FALSE
-net localgroup administrators USERNAME /add
-reg add "HKLM\SYSTEM\CurrentControlSet\Control\Terminal Server" /v fDenyTSConnections /T REG_DWORD /d 0 /f
-reg add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System /v EnableLUA /t REG_DWORD /d 0 /f 
-reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Terminal Server\WinStations\RDP-TCP" /v UserAuthentication /t REG_DWORD /d 0 /f
-netsh AdvFirewall set allprofiles state off
-net localgroup Administrators "Domain Users" /add
 "c:\BaseConfig\365install.exe" /configure "c:\BaseConfig\365config.xml"
 
 "c:\BaseConfig\adobeinstall.exe"
