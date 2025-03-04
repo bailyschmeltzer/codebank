8a01d3822d76bde87079b77a123297bfb734bece
bailyschmeltzer <cards45and12@gmail.com>
2025-03-04 09:17:26 -0500

    Update and rename BaseConfig.bat to AllowRunScripts.bat
    Changed name and set command to only allow running scripts

--- BaseConfig.bat
+++
@@ -1,9 +0,0 @@
-"c:\BaseConfig\365install.exe" /configure "c:\BaseConfig\365config.xml"
-
-"c:\BaseConfig\adobeinstall.exe"
-dism /online /Import-DefaultAppAssociations:"c:\BaseConfig\AppAssociations.xml"
-
-
-powershell.exe set-executionpolicy remotesigned
-powershell.exe -file "C:\BaseConfig\BaseConfig.ps1"
-
